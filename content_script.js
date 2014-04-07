036cea657db9548ec9d1cd9ef087a2e1b0309ea4
Brian Bergeron <bergeron@cmu.edu>
2014-04-07 01:33:42 -0400

    Fixes #7

--- content_script.js
+++ content_script.js
@@ -1,3 +1,21 @@
+var selected = ""
+
+if(window.getSelection().toString() != "")
+	selected = window.getSelection().toString();
+
+else{
+
+	for(var i=0; i < window.frames.length; i++){
+
+		if(window.frames[i].getSelection().toString() != ""){
+
+			selected = window.frames[i].getSelection().toString();
+			break;
+		}
+	}
+
+}
+
 
 //Send selected text to Rapid Reader extension
-chrome.runtime.sendMessage({selectedText:window.getSelection().toString()});
+chrome.runtime.sendMessage({selectedText:selected});
