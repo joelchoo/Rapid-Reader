8e6bd85906a74f6a7177ced872dcc1b66d2afefd
Brian Bergeron <bergeron@cmu.edu>
2014-04-24 06:43:07 -0400

    UI, Settings, ChunkSize fixes #3

---
+++ options.js
@@ -0,0 +1,41 @@
+
+
+// Execute content script as soon as DOM is loaded for extension box
+// (Each time icon is clicked)
+document.addEventListener('DOMContentLoaded', function () {
+
+	restore_options();
+	document.getElementById('save').addEventListener('click',save_options);
+	
+});
+
+
+
+
+
+// Saves options to chrome.storage
+function save_options() {
+  var wpm = document.getElementById('wpmSetting').value;
+  var chunkSize = document.getElementById('chunkSizeSetting').value;
+
+  chrome.storage.local.set({
+        'rapidReadWPM': wpm,
+        'rapidReadChunkSize': chunkSize
+    }, function(){
+
+    	window.location = "popup.html";
+    });
+
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+
+  chrome.storage.local.get(null, function (result) {
+
+    document.getElementById('wpmSetting').value = result.rapidReadWPM;
+    document.getElementById('chunkSizeSetting').value = result.rapidReadChunkSize;
+  
+});
+}
--- popup.js
+++ popup.js
@@ -1,16 +1,51 @@
 
-var initialSpeed = 200; // Initial speed in WPM
+var wordsPerMin; // Initial speed in WPM
+var chunkSize; //Number of words displayed at once
 var interval;
 var words;
 var index = 0;
 
 
+
+
 // Execute content script as soon as DOM is loaded for extension box
 // (Each time icon is clicked)
 document.addEventListener('DOMContentLoaded', function () {
-	initSlider();
+
+	//Cant do this till DOM loaded
+	document.getElementById('settingsBtn').addEventListener('click',editSettings);
+
+	  chrome.storage.local.get(null, function (result) {
+	
+	    if(result.rapidReadWPM == undefined || result.rapidReadChunkSize == undefined){
+
+	    	  chrome.storage.local.set({
+			        'rapidReadWPM': 200,
+			        'rapidReadChunkSize': 1
+			    });
+
+	    	wordsPerMin = 200;
+	    	chunkSize = 1;
+	    }
+	    else{
+
+	    	 wordsPerMin = result.rapidReadWPM;
+	   		 chunkSize = result.rapidReadChunkSize;
+	    }
+
+	    initSlider();
+
+  
+	});	
+
+
   	chrome.tabs.executeScript(null, {file: "content_script.js"});
-});
+
+  	document.getElementById("wpm").innerHTML = "WPM: "+wordsPerMin+"<br>"+"Chunks: "+chunkSize;
+
+
+	});
+	    
 
 
 /* Keyboard shortcut */
@@ -23,16 +58,24 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
   	words = request.selectedText.split(/\s/);
-  	readOnInterval(initialSpeed);
+  	readOnInterval(wordsPerMin);
     return false;
     
   });
+
+function editSettings(){
+
+	window.location  = "options.html";
+}
+
 
 
 // Creates slider, binds event listener that is called when slider changes value
 function initSlider(){
 
-	$( "#slider" ).slider({min:15},{max:600},{value:initialSpeed});
+
+
+	$( "#slider" ).slider({min:15},{max:600},{value:wordsPerMin});
 
 	$( "#slider" ).on( "slidechange", function( event, ui ) 
 		{
@@ -43,11 +86,24 @@ function initSlider(){
 		} );
 }
 
+function getWords(){
 
+	var concat = "";
+
+	for(var i=0; i < chunkSize; i++){
+
+		if(index >= words.length)
+			break;
+		
+		concat += words[index++]+" ";
+	}
+	
+	return concat;
+}
 
 function readOnInterval(speed){
 
-	document.getElementById("wpm").innerHTML = "WPM: "+speed;
+	document.getElementById("wpm").innerHTML = "WPM: "+speed+"<br>"+"Chunks: "+chunkSize;
 
 	interval = setInterval(function(){
 
@@ -57,7 +113,7 @@ function readOnInterval(speed){
 			return;
 		}
 
-		document.getElementById("words").innerHTML = words[index++];
+		document.getElementById("words").innerHTML = getWords();
 
 	},wpmToDelay(speed));
 
